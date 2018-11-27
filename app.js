ab3b4d8cab45c1bdfa294464dc693bbae24b54d4
Tanuj Chawla <chawla.tanuj96@gmail.com>
2018-11-27 23:33:35 +0530

    till 18:35

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
 
+//Set Storage Engine
+const storage = multer.diskStorage({
+    destination : './public/uploads/',
+    filename : function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+//Init Upload
+const upload = multer({
+    storage : storage
+}).single('myImage');
+
+//Init app
 const app = express();
 
+//EJS
 app.set('view engine', 'ejs');
 
+//Public Folder
 app.use(express.static('./public'));
 
-app.get('/', () => {
+app.get('/', (req, res) => {
     res.render('index');
 });
 
