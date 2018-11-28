2495aa1223645fbd7e8d5e2c0a291609f86e3193
Tanuj Chawla <tanuj.chawla@biz2credit.org>
2018-11-28 13:44:15 +0530

    completed

--- app.js
+++ app.js
@@ -13,8 +13,33 @@ const storage = multer.diskStorage({
 
 //Init Upload
 const upload = multer({
-    storage : storage
+    storage : storage,
+    limits : {
+        fileSize : 1000000
+    },
+    fileFilter : function(req, file, cb) {
+        checkFileType(file, cb);
+    }
 }).single('myImage');
+
+//Check file type
+
+function checkFileType(file, cb) {
+    //Allowed extensions
+    const filetypes = /jpeg|jpg|png|gif/;
+
+    // check extension
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    //check mimetype
+    const mimetype = filetypes.test(file.mimetype);
+
+    if(mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error : Images Only!');
+    }
+}
 
 //Init app
 const app = express();
@@ -27,6 +52,25 @@ app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if(err) {
+            res.render('index', {
+                msg : err
+            });
+        } else if(req.file == undefined){
+            res.render('index', {
+                msg : 'No file selected!'
+            });
+        } else {
+            res.render('index', {
+                msg : 'File Uploaded!',
+                file : `uploads/${req.file.filename}`
+            });
+        }
+    });
 });
 
 app.listen('3050', () => {
